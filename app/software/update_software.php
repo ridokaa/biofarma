a4a1816bd40470a189671b085b43feaa3172d886
icbyay <unyulucu22@gmail.com>
2022-08-31 09:39:37 +0700

    updt+thn

--- app/software/update_software.php
+++ app/software/update_software.php
@@ -28,12 +28,13 @@ $id_software    = $_GET['id_software'];
 $kode_software  = $_GET['kode_software'];
 $nama_software  = $_GET['nama_software'];
 $kode_vendor    = $_GET['kode_vendor'];
-$jenis_software   = $_GET['jenis_software'];
+$jenis_software = $_GET['jenis_software'];
+$tahun          = $_GET['tahun'];
 
 //var_dump($kode_departemen,$nama_departemen, $kode_divisi ); die;
 
 // masukkan ke database
-$query = "UPDATE software SET kode_software='$kode_software',nama_software='$nama_software',kode_vendor='$kode_vendor', jenis_software='$jenis_software' WHERE id_software='$id_software'";
+$query = "UPDATE software SET kode_software='$kode_software',nama_software='$nama_software',kode_vendor='$kode_vendor', jenis_software='$jenis_software', tahun='$tahun' WHERE id_software='$id_software'";
 
 $hasil = mysqli_query($koneksi, $query);
 
