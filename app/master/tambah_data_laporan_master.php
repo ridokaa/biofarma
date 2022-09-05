79c95862ebfe030e06c8c7e63f7fad036738d413
ridokaa <ridokaa422@gmail.com>
2022-09-05 15:23:16 +0700

    laporan detail upload file

--- app/master/tambah_data_laporan_master.php
+++ app/master/tambah_data_laporan_master.php
@@ -32,22 +32,77 @@ $kode_direktorat    = $_POST['kode_direktorat'];
 $id_personel        = $_POST['id_personel'];
 
 
+// FILE UPLOAD VAL PLAN
+// ambil data file
+$namaFile = $_FILES['file-val_plan']['name'];
+$namaSementara = $_FILES['file-val_plan']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "../../uploads/val_plan/";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+// FILE UPLOAD URS
+// ambil data file
+$namaFile = $_FILES['file-urs']['name'];
+$namaSementara = $_FILES['file-urs']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "../../uploads/urs/";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+// FILE UPLOAD OQ
+// ambil data file
+$namaFile = $_FILES['file-oq']['name'];
+$namaSementara = $_FILES['file-oq']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "../../uploads/oq/";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+
 
 
 //var_dump($kode_departemen,$nama_departemen, $kode_divisi ); die;
 
-// masukkan ke database
-$query = "INSERT INTO laporan_master ( no_laporan, tanggal_laporan, kode_departemen, kode_divisi, kode_direktorat, id_personel)    VALUES ('$no_laporan', '$tanggal_laporan', '$kode_departemen', '$kode_divisi','$kode_direktorat','$id_personel')";
-// var_dump($query);
-$hasil = mysqli_query($koneksi, $query);
+// // masukkan ke database
+// $query = "INSERT INTO laporan_master ( no_laporan, tanggal_laporan, kode_departemen, kode_divisi, kode_direktorat, id_personel)    VALUES ('$no_laporan', '$tanggal_laporan', '$kode_departemen', '$kode_divisi','$kode_direktorat','$id_personel')";
+// // var_dump($query);
+// $hasil = mysqli_query($koneksi, $query);
 
-// var_dump(mysqli_affected_rows($koneksi));
+// // var_dump(mysqli_affected_rows($koneksi));
 
 //cek keberhasilan pendambahan data
-if ($hasil == true) {
-    echo "Penambahan data berhasil";
-    header('location:../index.php?page=master&no_laporan=' . $no_laporan);
-} else {
-    echo "Penambahan data gagal!" . mysqli_connect_error($koneksi);
-    header('location:../index.php?page=master');
-}
+// if ($hasil == true) {
+//     echo "Penambahan data berhasil";
+//     header('location:../index.php?page=master&no_laporan=' . $no_laporan);
+// } else {
+//     echo "Penambahan data gagal!" . mysqli_connect_error($koneksi);
+//     header('location:../index.php?page=master');
+// }
--- app/master/tmbh_detail.php
+++ app/master/tmbh_detail.php
@@ -6,7 +6,7 @@
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <form class="form-horizontal" method="get" action="vendor/tambah_data_vendor.php">
+            <form class="form-horizontal" method="post" action="master/tambah_data_laporan_master.php" enctype="multipart/form-data">
             <div class="modal-body">
                     <!-- Horizontal Form -->
                     
@@ -15,47 +15,47 @@
                         <div class="form-group row">
                                 <label for="nama_equipment" class="col-sm-2 col-form-label">Nama Alat</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="nama_equipment" placeholder="nama equipment" name="nama_equipment"required>
+                                    <input type="text" class="form-control" id="nama_equipment" placeholder="nama equipment" name="nama_equipment">
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <label for="kode_equipment" class="col-sm-2 col-form-label">ID Alat</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="kode_equipment" placeholder="ID alat" name="kode_equipment"required>
+                                    <input type="text" class="form-control" id="kode_equipment" placeholder="ID alat" name="kode_equipment">
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <label for="nama_software" class="col-sm-2 col-form-label">Software</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="nama_software" placeholder="software" name="nama_software" required>
+                                    <input type="text" class="form-control" id="nama_software" placeholder="software" name="nama_software" >
                                 </div>
                             </div>
                               <div class="form-group row">
                                 <label for="nama_supplier" class="col-sm-2 col-form-label">Supplier</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="nama_supplier" placeholder="supplier" name="nama_supplier"required>
+                                    <input type="text" class="form-control" id="nama_supplier" placeholder="supplier" name="nama_supplier">
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <label for="nama_vendor" class="col-sm-2 col-form-label">Vendor</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="nama_vendor" placeholder="vendor" name="nama_vendor"required>
+                                    <input type="text" class="form-control" id="nama_vendor" placeholder="vendor" name="nama_vendor">
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <label for="val_plan" class="col-sm-2 col-form-label">Val. Plan</label>
-                                <input type="radio" id="val_plan" name="val_plan"required>
+                                <input type="radio" id="val_plan" name="val_plan">
                                 <label for="val_plan" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="val_plan" name="val_plan"required>
+                                <input type="radio" id="val_plan" name="val_plan">
                                 <label for="val_plan" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile">
+                                <input type="file" class="custom-file-input" id="customFile" name="file-val_plan">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
                             </div>
                             </div>
@@ -63,14 +63,14 @@
 
                             <div class="form-group row">
                                 <label for="urs" class="col-sm-2 col-form-label">URS</label>
-                                <input type="radio" id="urs" name="urs"required>
+                                <input type="radio" id="urs" name="urs">
                                 <label for="urs" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="urs" name="urs"required>
+                                <input type="radio" id="urs" name="urs">
                                 <label for="urs" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile">
+                                <input type="file" class="custom-file-input" id="customFile" name="file-urs">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
                             </div>
                             </div>
@@ -78,9 +78,9 @@
 
                             <div class="form-group row">
                                 <label for="protokol" class="col-sm-2 col-form-label">Protokol</label>
-                                <input type="radio" id="protokol" name="protokol"required>
+                                <input type="radio" id="protokol" name="protokol">
                                 <label for="protokol" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="protokol" name="protokol"required>
+                                <input type="radio" id="protokol" name="protokol">
                                 <label for="protokol" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -93,9 +93,9 @@
 
                             <div class="form-group row">
                                 <label for="iq" class="col-sm-2 col-form-label">IQ</label>
-                                <input type="radio" id="iq" name="iq"required>
+                                <input type="radio" id="iq" name="iq">
                                 <label for="iq" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="iq" name="iq"required>
+                                <input type="radio" id="iq" name="iq">
                                 <label for="iq" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -108,14 +108,14 @@
 
                             <div class="form-group row">
                                 <label for="oq" class="col-sm-2 col-form-label">OQ</label>
-                                <input type="radio" id="oq" name="oq"required>
+                                <input type="radio" id="oq" name="oq">
                                 <label for="oq" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="oq" name="oq"required>
+                                <input type="radio" id="oq" name="oq">
                                 <label for="oq" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile">
+                                <input type="file" class="custom-file-input" id="customFile" name="file-oq">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
                             </div>
                             </div>
@@ -123,9 +123,9 @@
 
                             <div class="form-group row">
                                 <label for="pq" class="col-sm-2 col-form-label">PQ</label>
-                                <input type="radio" id="pq" name="pq"required>
+                                <input type="radio" id="pq" name="pq">
                                 <label for="pq" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="pq" name="pq"required>
+                                <input type="radio" id="pq" name="pq">
                                 <label for="pq" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -138,9 +138,9 @@
 
                             <div class="form-group row">
                                 <label for="val_report" class="col-sm-2 col-form-label">Val. Report</label>
-                                <input type="radio" id="val_report" name="val_report"required>
+                                <input type="radio" id="val_report" name="val_report">
                                 <label for="val_report" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="val_report" name="val_report"required>
+                                <input type="radio" id="val_report" name="val_report">
                                 <label for="val_report" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -153,9 +153,9 @@
 
                             <div class="form-group row">
                                 <label for="change_kontrol" class="col-sm-2 col-form-label">Change kontrol</label>
-                                <input type="radio" id="change_kontrol" name="change_kontrol"required>
+                                <input type="radio" id="change_kontrol" name="change_kontrol">
                                 <label for="change_kontrol" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="change_kontrol" name="change_kontrol"required>
+                                <input type="radio" id="change_kontrol" name="change_kontrol">
                                 <label for="change_kontrol" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -168,9 +168,9 @@
 
                             <div class="form-group row">
                                 <label for="sop" class="col-sm-2 col-form-label">SOP</label>
-                                <input type="radio" id="sop" name="sop"required>
+                                <input type="radio" id="sop" name="sop">
                                 <label for="sop" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="sop" name="sop"required>
+                                <input type="radio" id="sop" name="sop">
                                 <label for="sop" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -183,9 +183,9 @@
 
                             <div class="form-group row">
                                 <label for="fda21_cprpart11_comly" class="col-sm-2 col-form-label">FDA CFR Part 11 Comply</label>
-                                <input type="radio" id="fda21_cprpart11_comly" name="fda21_cprpart11_comly"required>
+                                <input type="radio" id="fda21_cprpart11_comly" name="fda21_cprpart11_comly">
                                 <label for="fda21_cprpart11_comly" class="col-sm-2 col-form-label">Ada</label>
-                                <input type="radio" id="fda21_cprpart11_comly" name="fda21_cprpart11_comly"required>
+                                <input type="radio" id="fda21_cprpart11_comly" name="fda21_cprpart11_comly">
                                 <label for="fda21_cprpart11_comly" class="col-sm-2 col-form-label">Tidak Ada</label>
                             
                             <div class="form-group row">
@@ -199,7 +199,7 @@
                             <div class="form-group row">
                                 <label for="keterangan" class="col-sm-2 col-form-label">Keterangan</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="keterangan" placeholder="keterangan" name="keterangan"required>
+                                    <input type="text" class="form-control" id="keterangan" placeholder="keterangan" name="keterangan">
                                 </div>
                             </div>
 
@@ -222,3 +222,10 @@
         <!-- /.modal-dialog -->
       </div>
 
+      <script>
+// Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+</script>
