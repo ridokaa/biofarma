28603cd48310734202b93defaa944c88c3b31b93
icbyay <unyulucu22@gmail.com>
2022-09-08 14:45:20 +0700

    add dashboard and dashboard dummy

---
+++ app/dashboard/data_index_dashboard.php
@@ -0,0 +1,20 @@
+<?php
+    //Koneksikan dengan database
+    // include('../config/koneksi.php');
+    $host       = "localhost";
+    $user       = "root";
+    $pass       = "";
+    $database   = "softwarealat";
+
+    $koneksi = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
+
+    $query_departemen = "SELECT COUNT(*) AS total FROM departemen";
+    $hasil_departemen = mysqli_query($koneksi, $query_departemen);
+    $jumlah_departemen = mysqli_fetch_assoc($hasil_departemen);
+
+    $query_divisi = "SELECT COUNT(*) AS total FROM divisi";
+    $hasil_divisi = mysqli_query($koneksi, $query_divisi);
+    $jumlah_divisi = mysqli_fetch_assoc($hasil_divisi);
+
+
+?>
